ab0ba0e0d9d0543cbe7688f04e2a072145991190
Joseph Shea-Bianco <j.sheabianco@gmail.com>
2019-03-12 19:59:40 -0700

    Added domain checking to contentscript

--- contentscript.js
+++ contentscript.js
@@ -2,13 +2,26 @@ $(document).ready(function(){
         var links = document.getElementsByTagName("a");
         for (i=0;i<links.length;i++) {
                 links[i].title = "safe";
-                console.log(links[i].href);
+
+		target_domain = links[i].href.split("https://").pop().split("http://").pop().match(/[^\/]*\//)
+		domain = window.location.hostname;
+
+		// Check for international characters
+		for(j=0; j < links[i].href.length; j++){
+			if(links[i].href[j] > '\u007E'){
+                                links[i].title = "unsafe";
+                                break;
+			}
+		}
+				
+		// Checks for percent encoding
                 for(j=0; j < links[i].href.length; j++){
                         if(links[i].href[j] == "%"){
                                 links[i].title = "unsafe";
                                 break;
                         }
                 }
+
                 $(links[i]).tooltip();
         }
 });
